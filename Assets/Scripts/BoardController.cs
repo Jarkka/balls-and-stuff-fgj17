b64aad8be0cd07d2b8f88fa9d37e28df0323d8a7
Jarkka Salminen <jarkka.salminen@gmail.com>
2017-01-20 22:05:24 +0200

    Added: Base for left and right arrow keys controls but limits are needed

---
+++ Assets/Scripts/BoardController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardController : MonoBehaviour {
+
+	void Start () {
+		// Do da start
+	}
+
+	void FixedUpdate () {
+		float hor = Input.GetAxis ("Horizontal");
+		if (hor != 0) {
+			//Debug.Log (hor);
+			transform.Rotate(Vector3.forward * hor);
+		}
+
+		Debug.Log (gameObject.transform.localRotation.z);
+	}
+}
