9201a4356cc657a86c9eeaac1ebed355324c640c
Jarkka Salminen <jarkka.salminen@gmail.com>
2017-01-21 14:51:28 +0200

    Added: Visuals

---
+++ Assets/Scripts/ConstantRotatorController.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstantRotatorController : MonoBehaviour {
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (Vector3.left * 50 * Time.deltaTime, Space.Self);
+	}
+}
