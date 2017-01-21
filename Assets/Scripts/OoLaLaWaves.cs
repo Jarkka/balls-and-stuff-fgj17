83f701f546204890533a61990fc9bc8d5a97dcb9
Jesse Luoto <jesse.luoto@gmail.com>
2017-01-21 15:12:53 +0100

    Refactor waves to look cooler and be more adjustable

--- Assets/Scripts/OoLaLaWaves.cs
+++ Assets/Scripts/OoLaLaWaves.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class OoLaLaWaves : MonoBehaviour {
+
+	int maxBlocks = 50;
+	int startEffectFrom = 10;
+	float amplitude = 5.0f;
+	float spread = 10.0f;
 	
 	void Update () {
 		Vector3 parentLocalPosition = transform.parent.localPosition;
 		Vector3 localPosition = Vector3.zero;
 		float distanceToCamera = Mathf.Abs (parentLocalPosition.z - Camera.main.transform.position.z);
-		float multiplier = Mathf.Max(0, (distanceToCamera - 10) / 40);
-		localPosition.y = Mathf.Lerp(0, Mathf.Sin (distanceToCamera + parentLocalPosition.x) * 5.0f, multiplier);
-		localPosition.x = Mathf.Lerp(0, multiplier * parentLocalPosition.x * 10.0f, multiplier);
+		float multiplier = Mathf.Max(0, (distanceToCamera - startEffectFrom) / (maxBlocks - startEffectFrom));
+		localPosition.y = Mathf.Lerp(0, Mathf.Sin (distanceToCamera + parentLocalPosition.x) * amplitude, multiplier);
+		float direction = Mathf.FloorToInt (parentLocalPosition.x % 2) == 0 ? -1 : 1;
+		localPosition.x = Mathf.Lerp(0, multiplier * parentLocalPosition.x * spread * direction, multiplier);
 		transform.localPosition = localPosition;
 	}
 }
