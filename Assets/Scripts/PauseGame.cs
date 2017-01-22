f4ff8bc7de4194338e4cb7ac76e1eeb530283b07
Jarkka Salminen <jarkka.salminen@gmail.com>
2017-01-22 11:03:39 +0200

    Fixed: UI click issues

--- Assets/Scripts/PauseGame.cs
+++ Assets/Scripts/PauseGame.cs
@@ -14,42 +14,48 @@ public class PauseGame : MonoBehaviour {
 	public Sprite pauseImage;
 	public Sprite playImage;
 
+	private float tweenTime = 0.3f;
+
 	public void Awake() {
 		foreach (MaskableGraphic r in fadeTheseIn) {
 			r.CrossFadeAlpha (0.0f, 0.0f, true);
 		}
 		foreach (Button r in buttons) {
 			r.interactable = false;
+			r.GetComponent<Image> ().raycastTarget = false;
 		}
 	}
 
 	public void OnButtonClick() {
 		if (Time.timeScale == 0) {
 			Time.timeScale = 1;
-			Camera.main.GetComponent<Animator> ().CrossFade("PauseMenuBlurRev", 0.6f);
+			Camera.main.GetComponent<Animator> ().CrossFade("PauseMenuBlurRev", tweenTime);
 			foreach (Button r in buttons) {
 				r.interactable = false;
+				r.GetComponent<Image> ().raycastTarget = false;
 			}
 			foreach (MaskableGraphic r in fadeTheseOut) {
-				r.CrossFadeAlpha (1.0f, 0.6f, true);
+				r.CrossFadeAlpha (1.0f, tweenTime, true);
 			}
 			foreach (MaskableGraphic r in fadeTheseIn) {
-				r.CrossFadeAlpha (0.0f, 0.6f, true);
+				r.CrossFadeAlpha (0.0f, tweenTime, true);
 			}
 		} else {
 			Time.timeScale = 0;
-			Camera.main.GetComponent<Animator> ().CrossFade("PauseMenuBlur", 0.6f);
+			Camera.main.GetComponent<Animator> ().CrossFade("PauseMenuBlur", tweenTime);
 			foreach (Button r in buttons) {
 				r.interactable = true;
+				r.GetComponent<Image> ().raycastTarget = true;
 			}
 			foreach (MaskableGraphic r in fadeTheseOut) {
-				r.CrossFadeAlpha (0.0f, 0.6f, true);
+				r.CrossFadeAlpha (0.0f, tweenTime, true);
 			}
 			foreach (MaskableGraphic r in fadeTheseIn) {
-				r.CrossFadeAlpha (1.0f, 0.6f, true);
+				r.CrossFadeAlpha (1.0f, tweenTime, true);
 			}
 			foreach (Button r in disableButtons) {
 				r.interactable = false;
+				r.GetComponent<Image> ().raycastTarget = false;
 			}
 		}
 
